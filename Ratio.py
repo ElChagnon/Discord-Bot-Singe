fb415f663de247b7140a65ee18bb67a77d3168be
ElChagnon <92257859+ElChagnon@users.noreply.github.com>
2021-10-10 15:02:38 +0200

    Add files via upload

---
+++ Ratio.py
@@ -0,0 +1,26 @@
+import discord
+import time
+import datetime
+Client = discord.Client()
+@Client.event   
+async def on_ready():
+    print("Connected")
+    streaming=discord.Streaming(name="sinje qui tourne", url="https://www.sinje.com")
+    await Client.change_presence(activity=streaming)
+@Client.event
+async def on_message(message):
+    x=0
+    if message.author == Client.user:   
+        return
+    if message.content == "culotté":
+        logmess = "Command run at:"+str(datetime.datetime.now())+"\n"
+        f=open("log.txt", "a+")
+        f.write(logmess)
+        game=discord.Game(name="""Sinje""")
+        await Client.change_presence(activity=game)
+        while x < 1:
+            await message.channel.send("Ratio")
+            x=x+1
+        else:
+            return
+Client.run("ODc1ODUwOTIzNTAxNTYzOTA0.YRbhvA.M2FHOBt60ICTIZUWCH9qe3RFrBo")
---
+++ couille.py
@@ -0,0 +1,26 @@
+import discord
+import time
+import datetime
+Client = discord.Client()
+@Client.event   
+async def on_ready():
+    print("Connected")
+    streaming=discord.Streaming(name="sinje qui tourne", url="https://www.sinje.com")
+    await Client.change_presence(activity=streaming)
+@Client.event
+async def on_message(message):
+    x=0
+    if message.author == Client.user:   
+        return
+    if message.content == "sus":
+        logmess = "Command run at:"+str(datetime.datetime.now())+"\n"
+        f=open("log.txt", "a+")
+        f.write(logmess)
+        game=discord.Game(name="""sinje""")
+        await Client.change_presence(activity=game)
+        while x < 1:
+            await message.channel.send("https://cdn.discordapp.com/attachments/873547672328495215/877243710826483772/tact.mp4")
+            x=x+1
+        else:
+            return
+Client.run("ODc1ODUwOTIzNTAxNTYzOTA0.YRbhvA.M2FHOBt60ICTIZUWCH9qe3RFrBo")
